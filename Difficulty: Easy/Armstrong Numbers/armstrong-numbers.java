4ba0c26107bb51ee443222b5e60c8b3a81f704ca
KUMAR NISHANT <161965319+K-Nishant-18@users.noreply.github.com>
2025-01-20 00:59:41 +0530

    Update armstrong-numbers.java

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -1,27 +1,27 @@
 //{ Driver Code Starts
 // Initial Template for Java
-import java.io.*;
-import java.util.*;
+// import java.io.*;
+// import java.util.*;
 
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            int n = Integer.parseInt(read.readLine());
-            Solution ob = new Solution();
-            boolean flag = ob.armstrongNumber(n);
-            if (flag) {
-                System.out.println("true");
+// class GFG {
+//     public static void main(String args[]) throws IOException {
+//         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+//         int t = Integer.parseInt(read.readLine());
+//         while (t-- > 0) {
+//             int n = Integer.parseInt(read.readLine());
+//             Solution ob = new Solution();
+//             boolean flag = ob.armstrongNumber(n);
+//             if (flag) {
+//                 System.out.println("true");
 
-            } else {
-                System.out.println("false");
-            }
+//             } else {
+//                 System.out.println("false");
+//             }
 
-            System.out.println("~");
-        }
-    }
-}
+//             System.out.println("~");
+//         }
+//     }
+// }
 // } Driver Code Ends
 
 
