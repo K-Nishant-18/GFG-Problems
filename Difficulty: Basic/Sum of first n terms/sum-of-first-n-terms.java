e73807c29f7f8a047e738841968d0ca764870e5b
KUMAR NISHANT <161965319+K-Nishant-18@users.noreply.github.com>
2025-01-21 12:17:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of first n terms/sum-of-first-n-terms.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfSeries(N));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int sumOfSeries(int n) {
+        int sum=0;
+        if(n>0){
+        int sum1=sumOfSeries(n-1);
+        sum=(n*n*n)+sum1;
+        
+        }
+        return sum;
+    }
+}
