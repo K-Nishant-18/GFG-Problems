9a5feceb1712260d10e939fe112c380b5bea2b45
KUMAR NISHANT <161965319+K-Nishant-18@users.noreply.github.com>
2025-01-25 18:35:01 +0530

    Update check-if-array-is-sorted.java

--- Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.java
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.java
@@ -7,13 +7,14 @@ import java.util.*;
 
 class Solution {
     public boolean arraySortedOrNot(int[] arr) {
-        int expectation=1;
-        int reallity=1;
+        boolean check =true;
         for(int i=0; i<arr.length-1; i++){
-            if(arr[i]>arr[i+1])
-            reallity=0;
+            if(arr[i]>arr[i+1]){
+                check = false;
+                break;
+            }
         }
-        return (expectation==reallity);
+        return check;
     }
 }
 
