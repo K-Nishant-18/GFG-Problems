8f1b73135a9feee3c90db073470a1cf8906bcb3e
KUMAR NISHANT <me.knishant@gmail.com>
2025-07-16 19:43:44 +0530

    Update print-the-kth-digit.java

--- Difficulty: Basic/Print the Kth Digit/print-the-kth-digit.java
+++ Difficulty: Basic/Print the Kth Digit/print-the-kth-digit.java
@@ -14,3 +14,39 @@ class Solution {
         return digit;
     }
 }
+
+
+//Better Solution
+// • Always prefer fastPower()->[Binary Exponentiation] over Math.pow() When working with large exponents
+
+class Solution {
+    static long kthDigit(int a, int b, int k) {
+        long digit=0;
+        long num=fastPower(a,b);
+        
+        while(k!=0){
+            digit=num%10;
+            num=num/10;
+            k--;
+        }
+        
+        return digit;
+    }
+    
+    static long fastPower(int a, int b) {
+        long ans = 1;
+        long base = a;
+        while (b > 0) {
+            if(b%2==1) {
+                ans = ans*base;
+                b--;
+            }else{
+                base = base*base;
+                b = b/2;
+            }
+        }
+        
+        return ans;
+    }
+    
+}
